28f930d22540b5e4187e36c66c4fbea64bb44bde
winniecat777 <winniecat777@gmail.com>
2023-09-14 02:14:23 +0800

    add console.log

--- toTop-btn.js
+++ toTop-btn.js
@@ -5,11 +5,12 @@ let mybutton = document.getElementById("totop");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  console.log("scrollFunction")
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
-  }
+  } 
 }
 
 // When the user clicks on the button, scroll to the top of the document

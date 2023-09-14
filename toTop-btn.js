bf5e533071bc2ac8666e60d3c9090f6eaf8c1874
winniecat777 <winniecat777@gmail.com>
2023-09-14 11:54:06 +0800

    edit

--- toTop-btn.js
+++ toTop-btn.js
@@ -14,7 +14,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.getElementById('totop')[0].scrollTop = 0;
-  // document.body.scrollTop = 0; // For Safari
-  // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }

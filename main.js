9deb3dbbcd4bc83f4fabf938576b4beb4de88ebb
winniecat777 <winniecat777@gmail.com>
2023-09-14 21:21:23 +0800

    edit

--- main.js
+++ main.js
@@ -18,7 +18,9 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
